2a7a65e4d0cc170c131cd0b00942eef00e22c4d7
simranelpixala <simran@elpixala.in>
2024-07-05 10:28:04 +0530

    bucket config added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['college-dakhla-bucket.blr1.digitaloceanspaces.com'],
+      },
+};
 
 export default nextConfig;
