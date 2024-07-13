48e008d0229cccd908f3ccc6d4f7a52fda9bdfea
shivam7703 <shivamg7703@gmail.com>
2024-07-13 19:05:06 +0530

    shivam

--- src/components/dashboardSections/TabContent.tsx
+++ src/components/dashboardSections/TabContent.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Profile from "./profileForm/Profile";
+import { AppliedCollege } from "./appliedCollege/AppliedColleges";
+import { PendingCollege } from "./pendingApplication/PendingApplication";
+import { AccountSetting } from "./accountSetting/AccountSetting";
 
 const TabContent = ({ activeTab, setMobileMenu, mobileMenu }: any) => {
   return (
@@ -7,6 +10,28 @@ const TabContent = ({ activeTab, setMobileMenu, mobileMenu }: any) => {
       {/* Render components based on activeTab */}
       {activeTab?.label === "Profile" && (
         <Profile
+          tab={activeTab}
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+      )}
+
+{activeTab?.label === "Applied Colleges" && (
+        <AppliedCollege
+          tab={activeTab}
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+      )}
+      {activeTab?.label === "Pending Applications" && (
+        <PendingCollege
+          tab={activeTab}
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+      )}
+      {activeTab?.label === "Account Settings" && (
+        <AccountSetting
           tab={activeTab}
           mobileMenu={mobileMenu}
           setMobileMenu={setMobileMenu}
---
+++ src/components/dashboardSections/profileForm/StudyAbroad.tsx
@@ -0,0 +1,111 @@
+"use client";
+import React, { useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import { Button } from "@/components/Button";
+import { FaTrash } from "react-icons/fa";
+
+export function AbroadCollege() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      colleges: [{ college: "", status: "" }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "colleges",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleFormSubmit = async (data: any) => {
+    // setNextButtonState(true);
+    try {
+      console.log(data);
+    //   setNextButtonState(false);
+    } catch (err) {
+      console.log(err);
+    //   setNextButtonState(false);
+    }
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit(handleFormSubmit)}
+        className="grid grid-cols-1 gap-5"
+      >
+        <h2 className="text-xl font-bold">Abroad Colleges You Applied/Applying To:</h2>
+
+        {fields.map((field, index) => (
+          <div key={field.id} className="flex items-center gap-2 flex-wrap">
+            <div className="flex-grow space-y-2">
+              <select
+                className="mt-5 h-11 w-full rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm text-zinc-500 outline-none duration-200 focus:outline-orange-500"
+                {...register(`colleges.${index}.college`)}
+              >
+                <option value="" className="capitalize">
+                  Select College
+                </option>
+                {[
+                  "medium Achcha waala college",
+                  "Achcha waala college",
+                  "thoda achcha waala college",
+                  "Sabse achcha wala college",
+                  "other",
+                ].map((item) => (
+                  <option key={item} value={item} className="capitalize">
+                    {item}
+                  </option>
+                ))}
+              </select>
+              <select
+                className="mt-5 h-11 w-full rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm text-zinc-500 outline-none duration-200 focus:outline-orange-500"
+                {...register(`colleges.${index}.status`)}
+              >
+                <option value="" className="capitalize">
+                  Status
+                </option>
+                {["Primary", "Secondary", "Other"].map((item) => (
+                  <option key={item} value={item} className="capitalize">
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {fields.length > 1 && (
+              <Button
+                type="button"
+                variant="orange"
+                className="sm:ml-3 max-sm:mt-4 text-xl sm:!rounded-full !p-3 max-sm:w-full"
+                onClick={() => remove(index)}
+              >
+                <FaTrash />
+              </Button>
+            )}
+          </div>
+        ))}
+
+        <div className="flex justify-between">
+          <Button
+            variant="orange"
+            className="px-6 min-w-max"
+            onClick={() => append({ college: "", status: "" })}
+            type="button"
+          >
+            Add more
+          </Button>
+          <Button variant="orangeMain" type="submit">
+            Save
+          </Button>
+        </div>
+      </form>
+      {error && <p className="mt-5 text-center text-red-600">{error}</p>}
+    </div>
+  );
+}
