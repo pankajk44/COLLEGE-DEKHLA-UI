0459e732adbb7636c0f91277325f59e17867381b
pankaj <er.pankaj,k4@gmail.com>
2024-08-07 15:06:50 +0530

    fixes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -11,20 +11,22 @@ const loaderVariants = {
 export default function Loader() {
   return (
     <motion.div
-      className="flex-center z-[999999999] h-screen w-screen items-center justify-center overscroll-none bg-orange-300"
+      className="flex-center absolute inset-0 z-[999999999] h-screen w-full items-center justify-center overscroll-none bg-white"
       variants={loaderVariants}
       initial="visible"
       animate="hidden"
       exit="hidden"
-      transition={{ duration: 1 }} // Adjust the duration as needed
+      transition={{ duration: 0.5 }}
     >
-      <Image
-        src={headerLogo}
-        alt="loader"
-        width={500}
-        height={500}
-        className="h-20 w-full animate-bounce object-contain"
-      />
+      <div className="backgroundGradient flex-center h-full w-full">
+        <Image
+          src={headerLogo}
+          alt="loader"
+          width={500}
+          height={500}
+          className="h-20 w-full animate-bounce object-contain"
+        />
+      </div>
     </motion.div>
   );
 }
