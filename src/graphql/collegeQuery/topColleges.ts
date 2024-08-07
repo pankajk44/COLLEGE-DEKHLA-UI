ce054282d7635c69bac89cfc80be13981d152283
simranelpixala <simran@elpixala.in>
2024-08-07 14:11:04 +0530

    updated

--- src/graphql/collegeQuery/topColleges.ts
+++ src/graphql/collegeQuery/topColleges.ts
@@ -1,128 +1,109 @@
 import { gql } from "@apollo/client";
 
 export const getAllTopColleges = gql`
-query getAllTopColleges($page: Int, $pageSize: Int) {
-  colleges(
-    filters: { isTopCollege: { eq: true } }
-    pagination: { page: $page, pageSize: $pageSize }
-    sort: "topCollegeSequence:asc"
-  ) {
-    meta {
-      pagination {
-        total
+  query getAllTopColleges($page: Int, $pageSize: Int) {
+    colleges(
+      filters: { isTopCollege: { eq: true } }
+      pagination: { page: $page, pageSize: $pageSize }
+      sort: "topCollegeSequence:asc"
+    ) {
+      meta {
+        pagination {
+          total
+        }
       }
-    }
-    data {
-      id
-      attributes {
-        slug
-        breadCrumb
-        imageGallery {
-          images {
-query getAllTopColleges($page: Int, $pageSize: Int) {
-  colleges(
-    filters: { isTopCollege: { eq: true } }
-    pagination: { page: $page, pageSize: $pageSize }
-    sort: "topCollegeSequence:asc"
-  ) {
-    meta {
-      pagination {
-        total
-      }
-    }
-    data {
-      id
-      attributes {
-        slug
-        breadCrumb
-        imageGallery {
-          images {
+      data {
+        id
+        attributes {
+          slug
+          breadCrumb
+          imageGallery {
+            images {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+          }
+          collegeLogo {
             data {
+              id
               attributes {
                 url
               }
             }
           }
-        }
-        collegeLogo {
-          data {
-            id
-            attributes {
-              url
+          collegeName
+          description
+          location {
+            state {
+              data {
+                id
+                attributes {
+                  state
+                }
+              }
             }
-          }
-        }
-        collegeName
-        description
-        location {
-          state {
-            data {
-              id
-              attributes {
-                state
+            city {
+              data {
+                id
+                attributes {
+                  city
+                }
+              }
+            }
+            country {
+              data {
+                id
+                attributes {
+                  country
+                }
               }
             }
           }
-          city {
+          college_type {
             data {
               id
               attributes {
-                city
+                collegeType
               }
             }
           }
-          country {
+          affiliation {
             data {
               id
               attributes {
-                country
+                organization
               }
             }
           }
-        }
-        college_type {
-          data {
-            id
-            attributes {
-              collegeType
-            }
-          }
-        }
-        affiliation {
-          data {
-            id
-            attributes {
-              organization
-            }
-          }
-        }
-        navbars {
-          data {
-            id
-            attributes {
-              navItem
-            }
-          }
-        }
-        allCourses {
-          courseFee
-          courseFeeLabel
-          examName {
+          navbars {
             data {
               id
               attributes {
-                examName
+                navItem
               }
             }
           }
+          allCourses {
+            courseFee
+            courseFeeLabel
+            examName {
+              data {
+                id
+                attributes {
+                  examName
+                }
+              }
+            }
+          }
+          estYear
+          avgPackage
+          hightestPackage
+          topCollegeSequence
         }
-        estYear
-        avgPackage
-        hightestPackage
-        topCollegeSequence
       }
     }
   }
-}
-
 `;
